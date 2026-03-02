54ce4d256cfe75e8476c232a555f62b282aedd6e
sahilcingh <sahilcingh@gmail.com>
2026-03-03 00:36:50 +0530

    Made the patients button functional in the navbar 2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // 1. Make sure you import it at the top! (Adjust the path if your folder structure is different)
-import PatientsList from './components/PatientsList'; 
-import Home from './components/Home';
-import NewPatient from './components/NewPatient';
-import OldPatient from './components/OldPatient';
+import PatientsList from './pages/PatientsList'; 
+import Home from './pages/Home';
+import NewPatient from './pages/NewPatient';
+import OldPatient from './pages/OldPatient';
 // ... other imports ...
 
 function App() {
